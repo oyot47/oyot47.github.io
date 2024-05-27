9ed345214d52eeb5db103623786b4471b7f8a7ac
oyot47 <150551228+oyot47@users.noreply.github.com>
2024-05-28 00:17:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Repan";
+  sender = "Sandi";
 }
 
 let footer = document.getElementById("credit");
